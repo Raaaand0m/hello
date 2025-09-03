85d7831aee4a35dab0cbc5e2cc3cad2fe0db52ea
Raaaand0m <bcunningham9@huskers.unl.edu>
2025-09-02 19:52:57 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Ben Cunningham
+ *         bcunningham9@unl.edu
+ * Date: 2025/09/02
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("I love the School of Computing!\n");
+
+  return 0;
+}
